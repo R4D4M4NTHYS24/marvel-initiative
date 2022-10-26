a45a882b6693a8be7b8346e98fbbdaa2972c54ef
CamiloRG <camilorg2017@gmail.com>
2022-10-25 19:20:35 -0500

    finish build gallery properties
    finish build gallery properties and responsive design

--- js/views/randomView.js
+++ js/views/randomView.js
@@ -81,3 +81,54 @@ const obs = new IntersectionObserver(
 obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
+/*Gallery*/
+
+const btnCierra = document.querySelector("#btn-close");
+const btnAdelanta = document.querySelector("#btn-up");
+const btnRetrocede = document.querySelector("#btn-down");
+const imagenes = document.querySelectorAll(".gallery img");
+const lightbox = document.querySelector("#principal-container");
+const imagenActiva = document.querySelector("#img-active");
+let indiceImagen = 0;
+
+/*Abre el Lightbox*/
+
+const abreLightbox = (event) => {
+  imagenActiva.src = event.target.src;
+  lightbox.style.display = "flex";
+  indiceImagen = Array.from(imagenes).indexOf(event.target);
+};
+
+imagenes.forEach((imagen) => {
+  imagen.addEventListener("click", abreLightbox);
+});
+
+/*Cierra el Lightbox */
+
+btnCierra.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+/* Adelanta la imagen*/
+
+const adelantaImagen = () => {
+  if (indiceImagen === imagenes.length - 1) {
+    indiceImagen = -1;
+  }
+  imagenActiva.src = imagenes[indiceImagen + 1].src;
+  indiceImagen++;
+};
+
+btnAdelanta.addEventListener("click", adelantaImagen);
+
+/*Retrocede la Imagen*/
+
+const retrocederImagen = () => {
+  if (indiceImagen === 0) {
+    indiceImagen = imagenes.length;
+  }
+  imagenActiva.src = imagenes[indiceImagen - 1].src;
+  indiceImagen--;
+};
+
+btnRetrocede.addEventListener("click", retrocederImagen);
